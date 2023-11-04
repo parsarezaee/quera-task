4f0ef9d9ca44a69b672cd9f539078e9cb11271b5
parsarezaee <me.parsarezaei@gmail.com>
2023-11-04 14:56:55 +0330

    Add Update and Delete API for Transaction

--- task/transactions/urls.py
+++ task/transactions/urls.py
@@ -3,5 +3,7 @@ from . import views
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('list/', views.TransactionListAPIView.as_view(), name='transaction_list')
+    path('list/', views.TransactionListAPIView.as_view(), name='transaction_list'),
+    path('update/<int:pk>/', views.TransactionUpdateAPIView.as_view(), name='transaction_update'),
+    path('delete/<int:pk>/', views.TransactionDeleteAPIView.as_view(), name='transaction_delete')
 ]
--- task/transactions/views.py
+++ task/transactions/views.py
@@ -2,6 +2,9 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from .models import Transaction
 from .serializers import TransactionSerializer
+from django.core.exceptions import PermissionDenied
+
+
 
 class TransactionListAPIView(generics.ListCreateAPIView):
     serializer_class = TransactionSerializer
@@ -14,3 +17,36 @@ class TransactionListAPIView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         # Associate the transaction with the currently authenticated user
         serializer.save(user=self.request.user)
+
+
+class TransactionUpdateAPIView(generics.RetrieveUpdateAPIView):
+    serializer_class = TransactionSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        # Filter transactions based on the currently authenticated user
+        return Transaction.objects.filter(user=self.request.user)
+
+    def perform_update(self, serializer):
+        # Ensure the transaction belongs to the authenticated user
+        transaction = self.get_object()
+        if transaction.user == self.request.user:
+            serializer.save()
+        else:
+            raise PermissionDenied("You do not have permission to update this transaction.")
+
+
+class TransactionDeleteAPIView(generics.DestroyAPIView):
+    serializer_class = TransactionSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        # Filter transactions based on the currently authenticated user
+        return Transaction.objects.filter(user=self.request.user)
+
+    def perform_destroy(self, instance):
+        # Ensure the transaction belongs to the authenticated user
+        if instance.user == self.request.user:
+            instance.delete()
+        else:
+            raise PermissionDenied("You do not have permission to delete this transaction.")
